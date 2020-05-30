9e8b3c7359537d8fcc877bebabeae79b0f3ddaa3
Hitarth Gupta <hitarthgupta1@gmail.com>
2020-05-30 18:57:12 +0530

    Added Morphext and Animation css in hero h1 title

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
     Pace.on("done", function(){
         $(".pre_loader").css("display","none");
-    })
+    });
+
+    // $(".hero_title").each(function(i){
+    //     var $current_element = $(this);
+    //     setTimeout(function(){
+    //         $current_element.addClass("reveal");
+    //     }, i * 500);
+    // });
+
+
+    $("#rotating-text").Morphext({
+        // The [in] animation type. Refer to Animate.css for a list of available animations.
+        animation: "flip",
+        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+        separator: ",",
+        // The delay between the changing of each phrase in milliseconds.
+        speed: 2000,
+        complete: function () {
+        // Called after the entrance animation is executed.
+        }
+    });
+
+
 })
