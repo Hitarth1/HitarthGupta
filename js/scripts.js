55e3bc1bbcb4acf8fa93c2ef194ade1e279b3fc0
Hitarth Gupta <hitarthgupta1@gmail.com>
2020-05-30 19:25:29 +0530

    Added css animation to h1 title

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ $(document).ready(function(){
         $(".pre_loader").css("display","none");
     });
 
-    // $(".hero_title").each(function(i){
-    //     var $current_element = $(this);
-    //     setTimeout(function(){
-    //         $current_element.addClass("reveal");
-    //     }, i * 500);
-    // });
+    $(".hero_title").each(function(i){
+        var $current_element = $(this);
+        setTimeout(function(){
+            $current_element.addClass("reveal");
+        }, i * 500);
+    });
 
 
     $("#rotating-text").Morphext({
         // The [in] animation type. Refer to Animate.css for a list of available animations.
-        animation: "flip",
+        animation: "swing",
         // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
         separator: ",",
         // The delay between the changing of each phrase in milliseconds.
